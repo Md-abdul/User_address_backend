43be7676a97543c424f0589937603ccc773564d1
Md-abdul <mdabdulq62@gmail.com>
2024-09-21 21:44:08 +0530

    update routes

--- Server_Side/routes/userRoutes.js
+++ Server_Side/routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const AddressAddedUser = require('../controllers/addressController');
 const router = express.Router();
 
-// Route for registering a user and address
 router.post('/register', AddressAddedUser);
 
 module.exports = router;
